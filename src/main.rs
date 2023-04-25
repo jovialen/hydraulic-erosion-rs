20b8021529a2142b9e080b09864976180bf2c873
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-04-25 18:35:51 +0200

    feat: add egui window with config and diagnostics

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 mod orbit_camera;
 
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::pbr::prelude::*;
 use bevy::pbr::wireframe::*;
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use noise::{NoiseFn, Perlin};
 use orbit_camera::*;
 
 #[derive(Bundle, Default)]
 struct TerrainBundle {
-    #[cfg(feature = "wireframe")]
-    wireframe: Wireframe,
-
     #[bundle]
     pbr: PbrBundle,
 }
@@ -19,6 +17,7 @@ struct TerrainBundle {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
         .add_plugin(WireframePlugin)
         .add_plugin(OrbitCameraPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin)
@@ -26,6 +25,7 @@ fn main() {
         .add_startup_system(setup_camera)
         .add_startup_system(setup_lights)
         .add_startup_system(setup_terrain)
+        .add_system(config_system)
         .run();
 }
 
@@ -92,4 +92,47 @@ where
             *vertex = f(Vec3::from_array(*vertex)).to_array();
         }
     }
+}
+
+fn config_system(
+    mut contexts: EguiContexts,
+    mut wireframe_config: ResMut<WireframeConfig>,
+    diagnostics: Res<Diagnostics>,
+) {
+    egui::Window::new("Renderer").show(contexts.ctx_mut(), |ui| {
+        ui.set_min_width(200.0);
+
+        ui.label("Settings");
+        ui.checkbox(&mut wireframe_config.global, "Enable wireframe");
+
+        ui.add_space(10.0);
+
+        ui.label("Diagnostics");
+        ui.group(|ui| {
+            let fps = diagnostics
+                .get(FrameTimeDiagnosticsPlugin::FPS)
+                .expect("fps diagnostic");
+            let frame_count = diagnostics
+                .get(FrameTimeDiagnosticsPlugin::FRAME_COUNT)
+                .expect("frame count diagnostic");
+            let frame_time = diagnostics
+                .get(FrameTimeDiagnosticsPlugin::FRAME_TIME)
+                .expect("frame time diagnostic");
+
+            ui.label(format!(
+                "Frames per second: {} (avg {})",
+                fps.smoothed().unwrap_or(0.0).round() as i32,
+                fps.average().unwrap_or(0.0).round() as i32
+            ));
+            ui.label(format!(
+                "Time to render frame: {:.3}ms (avg {:.3}ms)",
+                frame_time.smoothed().unwrap_or(0.0),
+                frame_time.average().unwrap_or(0.0)
+            ));
+            ui.label(format!(
+                "Total frames: {}",
+                frame_count.value().unwrap_or(0.0).floor() as i32
+            ));
+        })
+    });
 }
